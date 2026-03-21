2d61a8facff1cbc3c18d175aa45a6eeec0430284
Vladimir Spirin <spirin.vladimir@gmail.com>
2026-03-21 16:18:13 +0700

    28% loaded binary dump

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const loader = require('uniswap-v2-loader')
-const dump_csv = require('path').join(__dirname, 'dump.csv')
+const dump = require('path').join(__dirname, 'dump')
 
 module.exports.load = (params = {}) => loader.load({
     factory: '0x1097053fd2ea711dad45caccc45eff7548fcb362',
-    filename: dump_csv,
+    filename: dump,
+    csv: false,
     ...params
 })
 
@@ -11,7 +12,8 @@ module.exports.subscribe = (callback, params = {}) => loader.subscribe(
     callback,
     {
         factory: '0x1097053fd2ea711dad45caccc45eff7548fcb362',
-        filename: dump_csv,
+        filename: dump,
+        csv: false,
         ...params
     }
 )
